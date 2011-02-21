eae666e73058b25ee60deec5be2ca5ba99e6e524
Gerard Paapu <sharkbrainguy@gmail.com>
2011-02-21 16:31:02 +1300

    restored the demo

--- demo/demo.js
+++ demo/demo.js
@@ -1,8 +1,7 @@
 $(function () {	
-    $("#Twitter").chorus("@boxerhockey",
-                         "@boxerhockey/webcomic-fellows");
-//    $("#Github").chorus("GH:sharkbrainguy/qorus");
-//    $("#HackerNews").chorus("HN:pg", "FF:paul");
-//    $("#Facebook").chorus("FB:BarackObama");
-//    $("#Buzz").chorus("BZ:sharkbrainguy");
+    $("#Twitter").chorus("@boxerhockey", "@boxerhockey/webcomic-fellows");
+    $("#Github").chorus("GH:sharkbrainguy/qorus");
+    $("#HackerNews").chorus("HN:pg", "FF:paul");
+    $("#Facebook").chorus("FB:BarackObama");
+    $("#Buzz").chorus("BZ:sharkbrainguy");
 });
