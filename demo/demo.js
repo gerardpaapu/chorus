e6b3c38f6bbff249e7fe63d1cf2f3288f6bba138
Gerard Paapu <sharkbrainguy@gmail.com>
2011-02-22 10:44:59 +1300

    restored the demo

--- demo/demo.js
+++ demo/demo.js
@@ -1,7 +1,7 @@
 $(function () {	
-    $("#Twitter").chorus("@@sharkbrain");
-//    $("#Github").chorus("GH:sharkbrainguy/qorus");
-//    $("#HackerNews").chorus("HN:pg", "FF:paul");
-//    $("#Facebook").chorus("FB:BarackObama");
-//    $("#Buzz").chorus("BZ:sharkbrainguy");
+    $("#Twitter").chorus("@@boxerhockey");
+    $("#Github").chorus("GH:sharkbrainguy/qorus");
+    $("#HackerNews").chorus("HN:pg", "FF:paul");
+    $("#Facebook").chorus("FB:BarackObama");
+    $("#Buzz").chorus("BZ:sharkbrainguy");
 });
