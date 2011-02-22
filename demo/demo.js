419036600312b3feea9ab68629433a4467ef68d7
Gerard Paapu <sharkbrainguy@gmail.com>
2011-02-22 13:47:18 +1300

    added some comments

--- demo/demo.js
+++ demo/demo.js
@@ -1,7 +1,17 @@
 $(function () {	
+    // Uses the @@username shortcut for ConversationTimeline
     $("#Twitter").chorus("@@boxerhockey");
+
+    // Follows commits to the master branch of *this* project
     $("#Github").chorus("GH:sharkbrainguy/qorus");
+
+    // Paul Graham on HackerNews
+    // Paul Bucheit on FriendFeed
     $("#HackerNews").chorus("HN:pg", "FF:paul");
+
+    // Barack Obama on Facebook
     $("#Facebook").chorus("FB:BarackObama");
+
+    // Follows me on Buzz... does anyone use buzz?
     $("#Buzz").chorus("BZ:sharkbrainguy");
 });
