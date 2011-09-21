ae7144a64033ad67557c62165ade12289ef03744
Gerard Paapu <sharkbrainguy@gmail.com>
2011-09-21 15:57:09 +1200

    fixed the demo

--- demo/demo.js
+++ demo/demo.js
@@ -12,6 +12,6 @@ $(function () {
     // Follows me on Buzz... does anyone use buzz?
     $("#Buzz").chorus("BZ:sharkbrainguy");
 
-    Chorus.setGooglePlusAPIKey();
+    Chorus.setGooglePlusAPIKey(Chorus.GOOGLE_PLUS_DEV_KEY);
     $("#GooglePlus").chorus("+108227062559465276408");
 });
